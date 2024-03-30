8b11ae94c515a12c3f658226b11a5ea41c265ea7
Le Nguyen Quang Minh <101281380+lnqminh3003@users.noreply.github.com>
2024-03-30 23:30:08 +0700

    fix navigate screen

--- Template Authentication/navigation/Navigation.tsx
+++ Template Authentication/navigation/Navigation.tsx
@@ -64,7 +64,7 @@ function Navigation() {
     });
   }, [uid]);
 
-  if (isLoading) return <LoadingLayout />;
+  if (isLoading && uid == "") return <LoadingLayout />;
   return (
     <NavigationContainer>
       {uid == "" ? <AuthStack /> : <AuthenticatedStack />}
